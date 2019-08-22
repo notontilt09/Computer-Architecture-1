b28df09a9584acd407594ac910e0a4b677db01dc
Daniel Weinman <namniewd@gmail.com>
2019-08-22 15:08:02 -0400

    day 4 mvp complete

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -6,6 +6,15 @@ HLT  = 0b00000001
 LDI  = 0b10000010
 PRN  = 0b01000111
 MUL  = 0b10100010
+PUSH = 0b01000101
+POP  = 0b01000110
+CALL = 0b01010000
+RET  = 0b00010001
+ADD  = 0b10100000
+
+
+
+SP = 7
 
 class CPU:
     """Main CPU class."""
@@ -15,6 +24,7 @@ class CPU:
         self.pc = 0
         self.ram = [0] * 256
         self.reg = [0] * 8
+        self.reg[SP] = 0xF4
 
     def load(self, filename):
         """Load a program into memory."""
@@ -31,7 +41,7 @@ class CPU:
                 self.ram[address] = val
                 address += 1
         
-        print(self.ram)
+        # print(self.ram)
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
@@ -40,7 +50,7 @@ class CPU:
             self.reg[reg_a] += self.reg[reg_b]
         elif op == "SUB":
             self.reg[reg_a] -= self.reg[reg_b]
-        elif op == MUL:
+        elif op == "MUL":
             self.reg[reg_a] *= self.reg[reg_b]
         else:
             raise Exception("Unsupported ALU operation")
@@ -69,6 +79,7 @@ class CPU:
         """Run the CPU."""
         running = True
         while running:
+            # self.trace()
             ir = self.ram[self.pc]
             operand_a = self.ram_read(self.pc + 1)
             operand_b = self.ram_read(self.pc + 2)
@@ -85,12 +96,35 @@ class CPU:
                 print(self.reg[operand_a])
                 self.pc += 2
             
-            elif ir == MUL:
-                self.alu(MUL, operand_a, operand_b)
+            elif ir == ADD:
+                self.alu("ADD", operand_a, operand_b)
                 self.pc += 3
             
+            elif ir == MUL:
+                self.alu("MUL", operand_a, operand_b)
+                self.pc += 3
+            
+            elif ir == PUSH:
+                self.reg[SP] -= 1
+                self.ram_write(self.reg[operand_a], self.reg[SP])
+                self.pc += 2
+            
+            elif ir == POP:
+                self.reg[operand_a] = self.ram[self.reg[SP]]
+                self.reg[SP] += 1
+                self.pc += 2
+            
+            elif ir == CALL:
+                self.reg[SP] -= 1
+                self.ram[self.reg[SP]] = self.pc + 2 
+                self.pc = self.reg[operand_a]
+                
+            elif ir == RET:
+                self.pc = self.ram_read(self.reg[SP])
+                self.reg[SP] += 1
+            
             else:
-                print('Error: Unknown instruction')
+                print(f'Error: Unknown instruction {ir} at {self.pc}')
                 sys.exit(1)
 
 
